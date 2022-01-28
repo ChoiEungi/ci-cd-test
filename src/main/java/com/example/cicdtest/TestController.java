32fbff8c1ea57895c173c83719058f1d78b0146e
grace_goose <choieungi@gm.gist.ac.kr>
2022-01-29 01:17:14 +0900

    Initial commit

---
+++ src/main/java/com/example/cicdtest/TestController.java
@@ -0,0 +1,18 @@
+package com.example.cicdtest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test;";
+    }
+}
